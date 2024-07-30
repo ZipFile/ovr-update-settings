47624850247391b2d50b6c0b6bf1c15ebcd9dfe5
ZipFile <zipfile.d@protonmail.com>
2024-07-30 21:21:08 +0300

    Add project files.

---
+++ ovr-update-settings.cpp
@@ -0,0 +1,12 @@
+﻿// ovr-update-settings.cpp : Defines the entry point for the application.
+//
+
+#include "ovr-update-settings.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ ovr-update-settings.h
@@ -0,0 +1,8 @@
+﻿// ovr-update-settings.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
